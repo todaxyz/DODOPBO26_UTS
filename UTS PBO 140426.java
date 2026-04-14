311b5e8241283e90843cadbc4c9f3fbcd458d8c6
Dodo Fansyuri Andrian <dotelabruh11@gmail.com>
2026-04-14 14:21:23 +0700

    Membuat class MadiunMotor dan MadiunMobil

--- UTS PBO 140426.java
+++ UTS PBO 140426.java
@@ -14,6 +14,31 @@ public abstract class LayananTransportasi {
             this.jarak = jarak;
         }
     }
-
 public abstract double hitungTarif();
+}
+
+public class MadiunMotor extends LayananTransportasi {
+    public MadiunMotor(String namaDriver, double jarak, double saldoUser) {
+    super(namaDriver, jarak, saldoUser);
+    }
+
+    @Override
+    public double hitungTarif() {
+    double biayaAdmin = 3000 + (23 * 100);
+    double tarifDasar = this.jarak * 5000;
+    return tarifDasar + biayaAdmin;
+    }
+}
+
+public class MadiunMobil extends LayananTransportasi {
+    public MadiunMobil(String namaDriver, double jarak, double saldoUser) {
+    super(namaDriver, jarak, saldoUser);
+    }
+
+    @Override
+    public double hitungTarif() {
+    double tarifDasar = this.jarak * 5000;
+    double biayaAdmin = 3000 + (23 * 100);
+return tarifDasar + biayaAdmin;
+    }
 }
