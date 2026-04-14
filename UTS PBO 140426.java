6d3999025dfa9f397ee65926521ef9509b12ea50
Dodo Fansyuri Andrian <dotelabruh11@gmail.com>
2026-04-14 14:28:38 +0700

    Menambah Logika Transaksi & Aturan Output untuk MadiunMotor dan Mobil

--- UTS PBO 140426.java
+++ UTS PBO 140426.java
@@ -17,28 +17,64 @@ public abstract class LayananTransportasi {
 public abstract double hitungTarif();
 }
 
+// File: MadiunMotor.java
 public class MadiunMotor extends LayananTransportasi {
+
     public MadiunMotor(String namaDriver, double jarak, double saldoUser) {
-    super(namaDriver, jarak, saldoUser);
+        super(namaDriver, jarak, saldoUser);
     }
 
     @Override
     public double hitungTarif() {
-    double biayaAdmin = 3000 + (23 * 100);
-    double tarifDasar = this.jarak * 5000;
-    return tarifDasar + biayaAdmin;
+        double tarifDasar = this.jarak * 2500;
+        double biayaAdmin = 3000 + (23 * 100);
+        return tarifDasar + biayaAdmin;
+    }
+
+    public void prosesTransaksi() {
+        System.out.println("=== Transaksi oleh [DODO FANSYURI ANDRIAN] - [254311023] ==="); ;
+        
+        double totalTarif = hitungTarif();
+        
+        if (this.saldoUser >= totalTarif) {
+            this.saldoUser -= totalTarif;
+            System.out.println("Nama Driver: " + this.namaDriver);
+            System.out.println("Jarak: " + this.jarak + " km");
+            System.out.println("Total Bayar: Rp " + totalTarif);
+            System.out.println("Sisa Saldo: Rp " + this.saldoUser);
+        } else {
+            System.out.println("Gagal: Saldo tidak mencukupi untuk perjalanan ini!");
+        }
     }
 }
 
 public class MadiunMobil extends LayananTransportasi {
+
     public MadiunMobil(String namaDriver, double jarak, double saldoUser) {
-    super(namaDriver, jarak, saldoUser);
+        super(namaDriver, jarak, saldoUser);
     }
 
     @Override
     public double hitungTarif() {
-    double tarifDasar = this.jarak * 5000;
-    double biayaAdmin = 3000 + (23 * 100);
-return tarifDasar + biayaAdmin;
+        double tarifDasar = this.jarak * 5000;
+        double biayaAdmin = 3000 + (23 * 100);
+        return tarifDasar + biayaAdmin;
+    }
+
+    public void prosesTransaksi() {
+
+        System.out.println("=== Transaksi oleh [DODO FANSYURI ANDRIAN] - [254311023] === ");
+        
+        double totalTarif = hitungTarif();
+        
+        if (this.saldoUser >= totalTarif) {
+            this.saldoUser -= totalTarif;
+            System.out.println("Nama Driver: " + this.namaDriver);
+            System.out.println("Jarak: " + this.jarak + " km");
+            System.out.println("Total Bayar: Rp " + totalTarif);
+            System.out.println("Sisa Saldo: Rp " + this.saldoUser);
+        } else {
+            System.out.println("Gagal: Saldo tidak mencukupi untuk perjalanan ini!");
+        }
     }
 }
